0636abeb24a02c36060ffdf80190c221cf53cde0
jasonhunter107 <jasonhunter107@gmail.com>
2018-05-22 01:58:23 -0400

    Added activities for different screens and implemented them

--- Wire Spooler/MainActivity.cs
+++ Wire Spooler/MainActivity.cs
@@ -1,4 +1,5 @@
 ﻿using Android.App;
+using Android.Content;
 using Android.Widget;
 using Android.OS;
 using Android.Support.V7.App;
@@ -14,6 +15,28 @@ namespace Wire_Spooler
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            var btnAuto = FindViewById<Button>(Resource.Id.button4);
+            var btnManual = FindViewById<Button>(Resource.Id.button5);
+            var btnAlarm = FindViewById<Button>(Resource.Id.button6);
+
+            btnAuto.Click += (s, e) =>
+            {
+                Intent nextActivity = new Intent(this, typeof(AutoActivity));
+                StartActivity(nextActivity);
+            };
+
+            btnManual.Click += (s, e) =>
+            {
+                Intent nextActivity = new Intent(this, typeof(ManualActivity));
+                StartActivity(nextActivity);
+            };
+
+            btnAlarm.Click += (s, e) =>
+            {
+                Intent nextActivity = new Intent(this, typeof(AlarmActivity));
+                StartActivity(nextActivity);
+            };
         }
     }
 }
